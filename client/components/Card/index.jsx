d9add0d37bebe41ac3beba4d45198a08864ee232
KamenSentai <cvt.alain@gmail.com>
2020-03-29 04:33:16 +0200

    feat(styles): initialize styles

---
+++ client/components/Card/index.jsx
@@ -0,0 +1,5 @@
+import styles from './style.module.scss';
+
+export default () => (
+  <div className={styles.container} />
+);
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,3 +1,5 @@
+import Card from '@/components/Card/';
+
 export default () => (
-  <div />
+  <Card />
 );
