3150b7740a74f65999008970edcf6e07d4e3bdb1
KamenSentai <cvt.alain@gmail.com>
2020-03-29 20:12:18 +0200

    feat(header): add returning button

--- client/components/Header/index.jsx
+++ client/components/Header/index.jsx
@@ -1,7 +1,32 @@
+import Link from 'next/link';
+import Icon from '@/components/Icon';
+import { useRouter } from 'next/router';
 import styles from './style.module.scss';
 
-const Header = () => (
-  <header className={styles.container}>{process.env.name}</header>
-);
+const Header = () => {
+  const { route } = useRouter();
+  const homeRoute = '/';
+  const isHome = homeRoute === route;
+
+  return (
+    <header className={[
+      styles.container,
+      !isHome && styles.hasLink,
+    ]
+      .filter((className) => !!className)
+      .join(' ')}
+    >
+      {!isHome && (
+        <Link href={homeRoute}>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a className={styles.link}>
+            <Icon name="Return" width="20px" />
+          </a>
+        </Link>
+      )}
+      <h1 className={styles.title}>{process.env.name}</h1>
+    </header>
+  );
+};
 
 export default Header;
