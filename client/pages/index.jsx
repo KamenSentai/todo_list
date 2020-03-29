ce8ef263bae9f479d85a6786762b20a2257a5f5c
KamenSentai <cvt.alain@gmail.com>
2020-03-29 17:06:39 +0200

    feat(favicon): add favicon

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Card from '@/components/Card/';
 import Container from '@/components/Container/';
 import Header from '@/components/Header/';
@@ -5,6 +6,15 @@ import Navigation from '@/components/Navigation/';
 
 export default () => (
   <Card>
+    <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#64cdfa" />
+      <meta name="msapplication-TileColor" content="#64cdfa" />
+      <meta name="theme-color" content="#64cdfa" />
+    </Head>
     <Container>
       <Header />
       <Navigation />
---
+++ client/pages/signup.jsx
@@ -0,0 +1,11 @@
+import Card from '@/components/Card/';
+import Container from '@/components/Container/';
+import Header from '@/components/Header/';
+
+export default () => (
+  <Card>
+    <Container>
+      <Header />
+    </Container>
+  </Card>
+);
