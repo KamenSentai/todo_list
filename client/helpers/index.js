637acd342f9b46eada8108401b06743a7d841111
KamenSentai <cvt.alain@gmail.com>
2020-03-30 20:05:11 +0200

    refactor(helpers): rewrite clearProps

--- client/helpers/index.js
+++ client/helpers/index.js
@@ -1,20 +1,34 @@
 /**
  * Clear falsy props
  * @param {Object} props
+ * @example
+ * // Let `width` equals '10px' and `height` equals whatever falsy value.
+ * // Then `width` is sent as a prop but not `height`.
+ * const Icon = ({ name, width, height }) => (
+ *   <svg viewBox="0 0 512 512" {...clearProps({ width, height })}>
+ *     {shapes[name]}
+ *   </svg>
+ * )
+ * @returns {Object}
  */
-export const clearProps = (props) => {
-  const result = {};
-
-  Object.entries(props).forEach(([key, value]) => {
-    if (value) result[key] = value;
-  });
-
-  return result;
-};
+export const clearProps = (props) => Object
+  .entries(props)
+  .reduce((prev, [key, value]) => ({
+    ...prev,
+    ...!!value && { [key]: value },
+  }), {});
 
 /**
  * Join a list of truthy class names
  * @param  {...String} classNames
+ * // If `isPrimary` is truthy, the result className is `${styles.button} ${styles.isPrimary}`.
+ * // Else the result className is only `${styles.button}`.
+ * const Button = ({ children, isPrimary }) => (
+ *   <button className={joinClassNames(styles.button, isPrimary && styles.isPrimary)}>
+ *     {children}
+ *   </button>
+ * )
+ * @returns {String}
  */
 export const joinClassNames = (...classNames) => classNames
   .filter((className) => !!className)
