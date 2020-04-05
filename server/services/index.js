d86fbefe7e7919619563badc8886a4e81acd0c59
KamenSentai <cvt.alain@gmail.com>
2020-04-05 02:13:55 +0200

    feat(services): build services

---
+++ server/services/index.js
@@ -0,0 +1,5 @@
+import linkService from './link.service';
+import userService from './user.service';
+import taskService from './task.service';
+
+export default [linkService, userService, taskService];
